0a2ba88876652488058d55b6844e771e0eb36a6f
DishaSinghh <dishasingh.e@gmail.com>
2025-07-11 19:48:39 +0530

    loop patterns for

---
+++ pattern.java
@@ -0,0 +1,56 @@
+public class pattern {
+    public static void main(String[] args) {
+        // rectangle 4x5
+//         for(int i=0;i<=3;i++){
+//             for(int j=0;j<=4;j++){
+//                 System.out.print("*");
+//             }
+//               System.out.println(" ");
+//         }
+
+        //hollow rectangle 4x5
+        // for(int i=0;i<=3;i++){
+        //     for(int j=0;j<=4;j++){
+        //         if ((i==1&&j==1)||(i==1&&j==2)||(i==1&&j==3)||(i==2&&j==1)||(i==2&&j==2)||(i==2&&j==3)) {
+        //             System.out.print(" ");
+        //         }
+        //         else{
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        //half pyramid
+        // for(int i=0;i<=3;i++){
+        //     for(int j=0;j<=4;j++){
+            //     if(i>=j){
+            //         System.out.print("*");
+            //     }                                           //can use i>=j or j<=i in loop
+            //     else{
+            //         System.out.print("");
+            //     }
+            // }
+            // System.out.println("");/
+
+
+            //inverted half
+        //     for(int i=0;i<=2;i++){
+        //          for(int j=3;j>=i;j--){
+        //             System.out.print("*");                     //?????????????but worked
+        //     }
+        //     System.out.println("");
+        // }
+        // System.out.print("*");
+
+
+        for(int i=0;i<=3;i++){
+            for(int j=i;j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
